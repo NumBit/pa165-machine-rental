df5b9f60668ea404cfb9785f07d67d85e042b89d
Peter Baltazarovič <43496304+NumBit@users.noreply.github.com>
2020-03-26 23:43:42 +0100

    rental dao and jpa

---
+++ pa165-dmbk-machine-rental-core/src/main/java/cz/muni/fi/pa165/dmbk/machinerental/rental/dao/Rental.java
@@ -0,0 +1,44 @@
+package cz.muni.fi.pa165.dmbk.machinerental.rental.dao;
+
+import cz.muni.fi.pa165.dmbk.machinerental.user.dao.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+/**
+ * Rental class representing Rental entity.
+ *
+ * @author Peter Baltazarovič
+ */
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "RENTAL")
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long  id;
+
+    @Column(nullable = false)
+    private LocalDate rentalDate;
+
+    @Column(nullable = false)
+    private LocalDate returnDate;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+//    @ManyToOne
+//    @JoinColumn(name = "machine_id", nullable = false)
+//    private Machine machine;
+
+    private String description;
+}
