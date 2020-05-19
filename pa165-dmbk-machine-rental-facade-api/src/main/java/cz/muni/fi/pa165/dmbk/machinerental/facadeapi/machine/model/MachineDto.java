2a28974269d95d61c670a797c48de4f00f919a86
Norbert Dopjera <ndopjera@gmail.com>
2020-05-19 11:27:52 +0200

    fix/dopjera - dozer mapping hotfix

--- pa165-dmbk-machine-rental-facade-api/src/main/java/cz/muni/fi/pa165/dmbk/machinerental/facadeapi/machine/model/MachineDto.java
+++ pa165-dmbk-machine-rental-facade-api/src/main/java/cz/muni/fi/pa165/dmbk/machinerental/facadeapi/machine/model/MachineDto.java
@@ -1,8 +1,10 @@
 package cz.muni.fi.pa165.dmbk.machinerental.facadeapi.machine.model;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.math.BigDecimal;
@@ -13,17 +15,16 @@ import java.math.BigDecimal;
  * @author Márius Molčány - UČO: 456350 - Github: overlordsvk
  */
 
-@Getter
-@ToString
-@EqualsAndHashCode
+@Data
+@NoArgsConstructor
 @JsonDeserialize(builder = MachineDto.Builder.class)
 public class MachineDto {
 
-    private final Long id;
-    private final String name;
-    private final String description;
-    private final String manufacturer;
-    private final BigDecimal price;
+    private Long id;
+    private String name;
+    private String description;
+    private String manufacturer;
+    private BigDecimal price;
 
     @lombok.Builder(builderClassName = "Builder", setterPrefix = "with")
     public MachineDto(Long id, String name, String description, String manufacturer, BigDecimal price) {
