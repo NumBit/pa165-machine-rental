4aa7b4b9f0cc28e7919cdea6ec5296454e81961c
overlordsvk <molcanym@gmail.com>
2020-05-20 22:41:40 +0200

    Contain instead of Like

--- pa165-dmbk-machine-rental-dao/src/main/java/cz/muni/fi/pa165/dmbk/machinerental/dao/machine/MachineRepository.java
+++ pa165-dmbk-machine-rental-dao/src/main/java/cz/muni/fi/pa165/dmbk/machinerental/dao/machine/MachineRepository.java
@@ -16,9 +16,9 @@ import java.util.List;
 public interface MachineRepository extends JpaRepository<Machine, Long> {
     List<Machine> findByName(String name);
 
-    List<Machine> findByNameLike(String name);
+    List<Machine> findByNameContaining(String name);
 
     List<Machine> findByManufacturer(String manufacturer);
 
-    List<Machine> findByManufacturerLike(String manufacturer);
+    List<Machine> findByManufacturerContaining(String manufacturer);
 }
